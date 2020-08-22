288a77d4869160216135f56ad2a48b41a968a33a
PeterGoedeke <peterbgoedeke@gmail.com>
2020-08-23 08:48:06 +1200

    Added processing for bulk API calls

--- tidely_parse.py
+++ tidely_parse.py
@@ -2,7 +2,14 @@ import datetime
 import functools
 
 # replace this with a bijective map and use a database
-locations = { '-36.59,174.68': 'Orewa' }
+locations = { '-36.59,174.68': 'Orewa', '-36.90,174.99': 'Golf course' }
+
+def processAPICallBulk(responseBulk, configurations):
+    response = parseResponseBulk(responseBulk)
+    def process(x):
+        return functools.reduce(lambda acc,cur: cur(x) + acc, configurations, [])
+    # process the response for each location and then flatten it
+    return [chunk for locationResponse in response for chunk in process(locationResponse)]
 
 def processAPICall(responseRaw, configurations):
     """Parses the response from the API call with parseResponse and then processes the result with the configurations given."""
@@ -11,6 +18,9 @@ def processAPICall(responseRaw, configurations):
     elements = functools.reduce(lambda acc,cur: cur(response) + acc, configurations, [])
     return elements
 
+def parseResponseBulk(responseBulk):
+    return [parseResponse(response) for response in responseBulk['area']]
+
 def parseResponse(responseRaw):
     """parses the response from the API call into a list of chunks which each have metadata attached."""
     def createMetadata(day, hourly):
@@ -18,11 +28,11 @@ def parseResponse(responseRaw):
             'time': toDatetime(day['date'], hourly['time']),
             'location': parseLocation(responseRaw)
         }
-    return [merge(hourly, createMetadata(day, hourly)) for day in responseRaw['data']['weather'] for hourly in day['hourly']]
+    return [merge(hourly, createMetadata(day, hourly)) for day in responseRaw['weather'] for hourly in day['hourly']]
 
 def parseLocation(response):
     """Parses the latitude and longitude from the API call and uses a cache to reverse-geocode it to be used as metadata"""
-    raw = response['data']['request'][0]['query']
+    raw = response['request'][0]['query']
     parsed = ','.join([elem[4:] for elem in raw.split(' and ')])
     return locations[parsed]
 
