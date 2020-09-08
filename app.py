46ac3a81048873ab3046e5e05519140b0921ef2d
Peter Goedeke <peterbgoedeke@gmail.com>
2020-09-08 22:01:54 +1200

    improvement(back): made the walking weather configuration slightly more realistic

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_cors import CORS, cross_origin
 
 import controllers
-from filters import WeatherConfiguration, BoundMatcher, TideMatcher
+from filters import WeatherConfiguration, BoundMatcher, TideMatcher, TimeMatcher
 from locations import nearbyLocations, nzlocations
 
 app = Flask(__name__)
@@ -10,7 +10,7 @@ cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 # example preset WeatherConfigurations for the user who is not logged in
-walking = WeatherConfiguration('Walking', [ BoundMatcher('FeelsLikeC', 12, 20), BoundMatcher('precipMM', 0, 0), TideMatcher(True, 120) ])
+walking = WeatherConfiguration('Walking', [ BoundMatcher('FeelsLikeC', 12, 20), BoundMatcher('precipMM', 0, 15), TideMatcher(True, 120), TimeMatcher(6, 10) ])
 rainCollecting = WeatherConfiguration('Rain collecting', [ BoundMatcher('FeelsLikeC', 0, 13), BoundMatcher('precipMM', 0.01, 10) ])
 
 @app.route('/<lat>,<lng>')
