07e657cdf18cfda8a8fdcd7a3db53a5209f928cc
Peter Goedeke <peterbgoedeke@gmail.com>
2020-09-23 15:25:58 +1200

    improvement(back): default recommendations are more realistic
    note: these are still not the weather recommendations we plan to ship later on; they will be reworked and improved in the future.

--- app.py
+++ app.py
@@ -2,16 +2,17 @@ from flask import Flask
 from flask_cors import CORS, cross_origin
 
 import controllers
-from filters import WeatherConfiguration, BoundMatcher, TideMatcher, TimeMatcher
+from filters import BoundMatcher, TideMatcher, TimeMatcher, TimeMatcher, WeatherCodeMatcher, WeatherConfiguration
 from locations import nearbyLocations, nzlocations
+import codes
 
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 # example preset WeatherConfigurations for the user who is not logged in
-walking = WeatherConfiguration('Walking', [ BoundMatcher('FeelsLikeC', 12, 20), BoundMatcher('precipMM', 0, 15), TideMatcher(True, 120), TimeMatcher(6, 10) ])
-rainCollecting = WeatherConfiguration('Rain collecting', [ BoundMatcher('FeelsLikeC', 0, 13), BoundMatcher('precipMM', 0.01, 10) ])
+walking = WeatherConfiguration('Walking', [ WeatherCodeMatcher(codes.calm) ])
+rainCollecting = WeatherConfiguration('Rain collecting', [ WeatherCodeMatcher(codes.raining) ])
 
 @app.route('/<lat>,<lng>')
 def getNearbyRecommendations(lat, lng):
