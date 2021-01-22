ee193dc9ef71d7e9faa6bb947ba21422e4113157
NicholasBatesNZ <NicholasBatesNZ@users.noreply.github.com>
2021-01-22 21:32:46 +1300

    tslint, refactor map dash, display marker data

--- app/static/ts/map.ts
+++ app/static/ts/map.ts
@@ -9,26 +9,39 @@ interface MarkerData {
     weatherRequirements: WeatherRequirement[];
 }
 
-navigator.geolocation.getCurrentPosition(async position => {
-    const map = L.map('map').setView([position.coords.latitude, position.coords.longitude], 13);
+class MapView {
+    private map: L.Map;
 
-    L.tileLayer('http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}', {
-        maxZoom: 20,
-        subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
-        attribution: 'Map data and Imagery &copy; Google'
-    }).addTo(map);
+    constructor(latitude: number, longitude: number) {
+        this.map = L.map('map').setView([latitude, longitude], 13);
 
-    map.on('click', (e: { latlng: L.LatLngExpression; }) => {
-        
-        const markerData: MarkerData = {
-            marker: L.marker(e.latlng).addTo(map),
-            activities: ['Rain Collecting'],
-            weatherRequirements: [{
-                weather: 'wind',
-                requirement: 'low'
-            }]
-        };
+        L.tileLayer('http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}', {
+            maxZoom: 20,
+            subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+            attribution: 'Map data and Imagery &copy; Google'
+        }).addTo(this.map);
 
-        markerData.marker.bindPopup('<b>' + markerData.activities[0] + '</b>').openPopup();
-    });
-});
+        this.map.on('click', (e: { latlng: L.LatLngExpression; }) => {
+            this.addMarkerDataToMap({
+                marker: L.marker(e.latlng),
+                activities: [prompt('activity', 'Rain Collecting')],
+                weatherRequirements: [{
+                    weather: prompt('weather', 'rain'),
+                    requirement: prompt('requirement', 'heavy')
+                }]
+            });
+        });
+    }
+
+    public addMarkerDataToMap(markerData: MarkerData): void {
+        markerData.marker.addTo(this.map).bindPopup(`<p>${markerData.marker.getLatLng()}</p>
+            <p>${markerData.activities[0]}</p>
+            <p>${markerData.weatherRequirements[0].weather}:
+            ${markerData.weatherRequirements[0].requirement}</p>`).openPopup();
+    }
+}
+
+navigator.geolocation.getCurrentPosition(
+    async position => new MapView(position.coords.latitude, position.coords.longitude),
+    () => new MapView(-36.8509, 174.7645)
+);
